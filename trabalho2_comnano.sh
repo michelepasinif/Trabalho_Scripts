000050bc8df8304996f4cf6e3da7d00ac633dadf
michelepasinif <michele.pasinif@gmail.com>
2016-10-05 20:09:14 -0300

    Update trabalho2_comnano.sh

--- trabalho2_comnano.sh
+++ trabalho2_comnano.sh
@@ -1,15 +1,27 @@
 #!/bin/sh
 
+#Desenvolvido por Michele França e Hermes 
+#Este script irá instalar o SSH-Server, alterar a porta e testar uma conexão para outra máquina 
+
+#Instalando o Openssh-server 
+
 apt-get install openssh-server
 
 echo SSH instalando 
+
+#Alterando a porta do SSH para 10000
+
 echo trocando a porta SSHD para 10000
 
 nano "/etc/ssh/sshd_config"
 
+#Echo reiniciando o SSH 
+
 echo Reiniciando o SSH 
 
 /etc/init.d/ssh restart 
+
+#Irá testar a conexão com a máquina do colega 
 
 echo Testando conexão no computador do colega 
 echo Digite o ip do colega 
