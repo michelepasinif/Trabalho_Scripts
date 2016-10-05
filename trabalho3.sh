4d255409400411c05d2979833bb5d3c363136e0a
michelepasinif <michele.pasinif@gmail.com>
2016-10-05 20:12:41 -0300

    Update trabalho3.sh

--- trabalho3.sh
+++ trabalho3.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
+#Script desenvolvido por Michele Pasini e Hermes 
+#Este script irá verificar se o sistema é 32/64 bis, e irá verificar detalhes do sistema operacional 
+
+#Exibe se o sistema é 32/64 bits 
 echo Verificando se o computador é 32 ou 64 bits 
 uname -m  
 
+#Verificando detalhes do sistema 
 echo Verificando os detalhes do sistema 
 cat /etc/issue 
 
