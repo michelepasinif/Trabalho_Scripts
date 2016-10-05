2a098a167933c8ee4638b1368b4a26642535c91d
michelepasinif <michele.pasinif@gmail.com>
2016-10-05 20:07:14 -0300

    Update trabalho1.sh
    Correções orientadas pelo professor

--- trabalho1.sh
+++ trabalho1.sh
@@ -1,13 +1,20 @@
 #!/bin/sh
 
+#Desenvolvido por Michele Pasini e Hermes 
+
+#Este script irá configurar a máquina para pegar IP por DHCP, alterar o nome do host e exibi-lo ao usuário 
+
+# IP por DHCP 
 echo Pegando IP por DHCP 
 
 ip a 
 
+#Irá alterar o nome da máquina 
 echo Alterando o nome da máquina  
 echo Digite o nome da máquina 
 read nome 
 
+#Exibe o nome da máquina para o usuário 
 hostname $nome
 echo exibe o nome da máquina 
 hostname   
