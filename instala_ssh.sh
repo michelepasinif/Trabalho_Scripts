47bf16ce41f38e0a914f145b2a5912a4845f22b8
michelepasinif <michele.pasinif@gmail.com>
2016-10-19 19:36:55 -0200

    Outubro de 2016

--- instala_ssh.sh
+++ instala_ssh.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
-#Desenvolvido por Michele França e Hermes 
+#Desenvolvido por Michele França e Hermes, Outubro de 2016
+
 #Este script irá instalar o SSH-Server, alterar a porta e testar uma conexão para outra máquina 
 
 #Instalando o Openssh-server 
 
 apt-get install openssh-server
 
-echo SSH instalando 
+echo "SSH instalando" 
 
 #Fazendo cópia do arquivo de configuração do SSH
 
@@ -15,11 +16,11 @@ cp "/etc/ssh/sshd_config" "/etc/ssh/sshd_config_bk"
 
 #Alterando a porta do SSH para 10000 com o comando sed 
 
-echo trocando a porta SSHD para 10000
+echo "trocando a porta SSHD para 10000, por favor aguarde"
 
 sudo sed -i "s/Port 22/Port 10000/g" /etc/ssh/sshd_config
 
-#Echo reiniciando o SSH 
+#Reiniciando o SSH
 
 echo "Reiniciando o SSH" 
 
@@ -28,17 +29,17 @@ echo "Reiniciando o SSH"
 #Capturando informações para a conexão ssh 
 
 echo "Digite o ip do colega" 
-read nome 
+  read nome 
 echo "Digite a port" 
-read port
+  read port
 
 
-#Irá testar a conexão com a máquina do colega 
+#Irá testar a conexão com a máquina do colega de acordo com os parâmetros inseridos pelo usuário 
 
 echo "Testando conexão no computador do colega" 
 echo "Digite o ip do colega" 
-read nome 
+      read nome 
 echo "Digite a port" 
-read port
+      read port
 
 ssh aluno@$nome -p $port
