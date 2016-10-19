11907838399d58af936db55434e20d5a0a5719fb
michelepasinif <michele.pasinif@gmail.com>
2016-10-18 22:04:44 -0200

    Update instala_ssh.sh

--- instala_ssh.sh
+++ instala_ssh.sh
@@ -34,21 +34,6 @@ echo Digite a port
 read port
 
 
-#Executando o comando para verificação das propriedades do sistema operacional via ssh
-
-ssh aluno@$nome 
-
-#Exibe se o sistema é 32/64 bits 
-echo Verificando se o computador é 32 ou 64 bits 
-uname -m  
-
-#Verificando detalhes do sistema 
-
-echo Verificando os detalhes do sistema 
-
-cat /etc/issue 
-
-
 #Irá testar a conexão com a máquina do colega 
 
 echo Testando conexão no computador do colega 
