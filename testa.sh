66f8f48559fdbae2e2f46ea744d2eb44a343d567
michelepasinif <michele.pasinif@gmail.com>
2016-10-18 22:08:02 -0200

    Update para o Trabalho 2

--- testa.sh
+++ testa.sh
@@ -3,12 +3,17 @@
 #Script desenvolvido por Michele Pasini e Hermes 
 #Este script irá verificar se o sistema é 32/64 bis, e irá verificar detalhes do sistema operacional 
 
+#Executando o comando para verificação das propriedades do sistema operacional via ssh
+
+ssh aluno@$nome 
+
 #Exibe se o sistema é 32/64 bits 
 echo Verificando se o computador é 32 ou 64 bits 
 uname -m  
 
 #Verificando detalhes do sistema 
-echo Verificando os detalhes do sistema 
-cat /etc/issue 
 
+echo Verificando os detalhes do sistema 
+
+cat /etc/issue 
 
